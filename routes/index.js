2b8d464fdc46a0e7f6d2a6157dc54d0feb1039e4
ecarreraz <e.carreraz@alumnos.upm.es>
2017-06-06 21:48:21 +0200

    p52

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -10,9 +12,36 @@ router.get('/', function(req, res, next) {
 router.get('/author', function(req, res, next) {
     res.render('author');
 });
+
+// Pagina del Juego
+router.get('/quizzes/random_play', function(req, res, next) {
+    res.render('random_play');
+});
+
+/*
 // Pagina de Ayuda
 router.get('/help', function(req, res, next) {
     res.render('help');
 });
+
+*/
+
+// Autoload de rutas que usen :quizId
+router.param('quizId', quizController.load);
+
+// Definición de rutas de /quizzes
+router.get('/quizzes',                     quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       quizController.show);
+router.get('/quizzes/new',                 quizController.new);
+router.post('/quizzes',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+
+router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+
+
+
 
 module.exports = router;
